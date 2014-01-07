e94a0e0ffb3d6a7fc27c157f495bd350a24b91a8
hawkfalcon <hawkfalcon1@gmail.com>
2014-01-07 11:47:29 -0800

    Update, villagers, blacklist

--- src/main/java/com/hawkfalcon/tse/Main.java
+++ src/main/java/com/hawkfalcon/tse/Main.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -14,7 +13,6 @@ import org.bukkit.material.SpawnEgg;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
-import java.util.Random;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -31,21 +29,23 @@ public class Main extends JavaPlugin implements Listener {
         if (player.hasPermission("tse.use")) {
             if ((event.getAction().equals(Action.RIGHT_CLICK_AIR)) || (event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
                 ItemStack item = event.getItem();
-                    if (!(item.getData() instanceof SpawnEgg) || item == null) return;
-                    SpawnEgg segg = (SpawnEgg) item.getData();
-                    if (this.getConfig().getStringList("blacklist").contains(segg.getSpawnedType().toString().toLowerCase()))
+                if (!(item.getData() instanceof SpawnEgg) || item == null) return;
+                SpawnEgg segg = (SpawnEgg) item.getData();
+                if (this.getConfig().getBoolean("blacklist")) {
+                    if (this.getConfig().getStringList("blacklisted").contains(segg.getSpawnedType().toString().toLowerCase()))
                         return;
-                    Egg egg = event.getPlayer().launchProjectile(Egg.class);
-                    this.eggs.put(egg, segg.getSpawnedType());
-                    GameMode gm = event.getPlayer().getGameMode();
-                    if (gm.equals(GameMode.SURVIVAL) || gm == GameMode.ADVENTURE) {
-                        if (item.getAmount() > 1) {
-                            item.setAmount(item.getAmount() - 1);
-                        } else {
-                            player.getInventory().remove(item);
-                        }
+                }
+                Egg egg = event.getPlayer().launchProjectile(Egg.class);
+                this.eggs.put(egg, segg.getSpawnedType());
+                GameMode gm = event.getPlayer().getGameMode();
+                if (gm.equals(GameMode.SURVIVAL) || gm == GameMode.ADVENTURE) {
+                    if (item.getAmount() > 1) {
+                        item.setAmount(item.getAmount() - 1);
+                    } else {
+                        player.getInventory().remove(item);
                     }
-                    event.setCancelled(true);
+                }
+                event.setCancelled(true);
             }
         }
     }
@@ -57,14 +57,17 @@ public class Main extends JavaPlugin implements Listener {
             EntityType entityType = this.eggs.get(egg);
             Entity entity = egg.getWorld().spawnEntity(egg.getLocation(), entityType);
             if (entityType == EntityType.HORSE) {
-                Horse horse = (Horse)entity;
-                horse.setStyle(Horse.Style.values()[(int)(Math.random()*(Horse.Style.values().length))]);
+                Horse horse = (Horse) entity;
+                horse.setStyle(Horse.Style.values()[(int) (Math.random() * (Horse.Style.values().length))]);
                 horse.setColor(Horse.Color.values()[(int) (Math.random() * (Horse.Color.values().length))]);
             } else if (entityType == EntityType.SHEEP) {
                 if (!getConfig().getBoolean("coloredsheep")) return;
-                ((Sheep)entity).setColor(DyeColor.values()[(int) (Math.random() * (DyeColor.values().length))]);
+                ((Sheep) entity).setColor(DyeColor.values()[(int) (Math.random() * (DyeColor.values().length))]);
+            } else if (entityType == EntityType.VILLAGER) {
+                Villager villager = (Villager) entity;
+                villager.setProfession(Villager.Profession.values()[(int) (Math.random() * (Villager.Profession.values().length))]);
             }
-                event.setHatching(false);
+            event.setHatching(false);
         }
     }
 }
